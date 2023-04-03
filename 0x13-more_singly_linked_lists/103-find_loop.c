930bbefd4eebcacde8b80c482f76e0793b9c1b6f
Avo-Teddy803 <amanosamuel803@gmail.com>
2023-04-03 16:54:18 -0700

    0x13 C More Singly Linked Lists

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,49 +1,32 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
  * find_listint_loop - finds the loop in a linked list.
  * @head: head of a list.
  *
- * Return: the address of the node where the loop starts.
+ * Return: the address of the node where the loop starts,
+ * or NULL if there is no loop.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *p2;
-    listint_t *prev;
-    listint_t *tmp;
+	listint_t *p1 = head, *p2 = head;
 
-    if (!head)
-        return (NULL);
+	while (p1 && p2 && p2->next)
+	{
+		p1 = p1->next;
+		p2 = p2->next->next;
 
-    p2 = head;
-    prev = head;
-    while (head && p2 && p2->next)
-    {
-        head = head->next;
-        p2 = p2->next->next;
+		if (p1 == p2)
+		{
+			p1 = head;
+			while (p1 != p2)
+			{
+				p1 = p1->next;
+				p2 = p2->next;
+			}
+			return (p1);
+		}
+	}
 
-        if (head == p2)
-        {
-            head = prev;
-            prev =  p2;
-            while (1)
-            {
-                p2 = prev;
-                while (p2->next != head && p2->next != prev)
-                {
-                    p2 = p2->next;
-                }
-                if (p2->next == head)
-                    break;
-
-                head = head->next;
-            }
-            tmp = p2->next;
-            p2->next = NULL;
-            return (tmp);
-        }
-    }
-
-    return (NULL);
+	return (NULL);
 }
