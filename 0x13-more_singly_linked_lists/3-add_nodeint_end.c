659d45ca51a6169aef3807b8d619309bb72c144f
Avo-Teddy803 <amanosamuel803@gmail.com>
2023-04-04 15:52:57 -0700

    0x13 C More Singly Linked Lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,36 +1,35 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to pointer to head of listint_t list
- * @n: integer to store in new node
+ * add_nodeint_end - adds a new node at the end of a listint_t list.
+ * @head: pointer to the head of the list.
+ * @n: integer value to be stored in the new node.
  *
- * Return: address of new element, or NULL on failure
+ * Return: address of the new element, or NULL if it failed.
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new_node, *temp;
+	listint_t *new_node, *temp;
 
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
+	if (!head)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->next = NULL;
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-    }
-    else
-    {
-        temp = *head;
-        while (temp->next != NULL)
-        {
-            temp = temp->next;
-        }
-        temp->next = new_node;
-    }
+	new_node->n = n;
+	new_node->next = NULL;
 
-    return (new_node);
+	if (!*head)
+		*head = new_node;
+	else
+	{
+		temp = *head;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new_node;
+	}
+
+	return (new_node);
 }
